945d84ccee87ffbaafa24a250da3ec2698f967d5
garcia808 <garcia.ga@outlook.com>
2020-10-24 22:47:45 -0500

    añadiendo solución lab numpy-deep-dive

--- your-code/main.py
+++ your-code/main.py
@@ -1,67 +1,72 @@
 #1. Import the NUMPY package under the name np.
-
+import numpy as np
 
 
 #2. Print the NUMPY version and the configuration.
-
+np.version.version
+np.show_config()
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
-
+a=np.random.random((2,3,5))
 
 
 #4. Print a.
-
+print(a)
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
+b=np.full([5,2,3],1)
 
 
 #6. Print b.
-
+print(b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
-
+print(a.size==b.size)
+print('Yes')
 
 
 
 #8. Are you able to add a and b? Why or why not?
-
+print(np.add(a,b))
+print('No, I could not add a and b because: ValueError: operands could not be broadcast together with shapes (2,3,5) (5,2,3)')
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
+c=np.transpose(b)
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
+d=np.add(a,c)
+print('It does not work. ValueError: operands could not be broadcast together with shapes (2,3,5) (3,2,5)')
 
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-
+print(a)
+print('It is not possible to prind d')
 
 
 
 #12. Multiply a and c. Assign the result to e.
-
-
+e=np.multiply(a,c)
+print('ValueError: operands could not be broadcast together with shapes (2,3,5) (3,2,5)')
 
 #13. Does e equal to a? Why or why not?
-
+print('No, it is not possible to obtain e')
 
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
-
+print('It is not possible to obtain d')
 
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
-
+f=np.empty((2,3,5))
 
 
 
@@ -74,7 +79,7 @@ Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
-
+print('It is not possible to obtain d')
 
 
 
@@ -98,7 +103,7 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
-
+print('It is not possible to obtain d')
 
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
