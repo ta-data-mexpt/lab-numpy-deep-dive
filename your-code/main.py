516101f70de7b5aba16875ee40440bf0391b56d6
Nayely Saldivar <nay.pffy@gmail.com>
2020-10-25 23:30:38 -0600

    Lab de Numpy listo

--- your-code/main.py
+++ your-code/main.py
@@ -1,68 +1,79 @@
 #1. Import the NUMPY package under the name np.
 
-
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
-
+print (np.__version__)
+print (np.__config__)
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
-
+# a=np.empty((3,2,5))
+a=np.random.random((2,3,5))
 
 #4. Print a.
 
-
+print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b=np.ones((5,2,3))
 
 #6. Print b.
 
-
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
-
+a.size==b.size
 
 #8. Are you able to add a and b? Why or why not?
 
-
+# No por que no tienen las mismas dimensiones
+a+b 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
-
+c=np.transpose(b,(1,2,0))
+c
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+# Se pueden sumar porque comparten la estructura nxmxo y la operación se realiza elemento por elemento
+d=a+c
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
-
-
+# A todos los elementos de a se les añadió la unidad que estaba en d
+print(a)
+print(d)
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e=np.multiply(a,c)
 
 #13. Does e equal to a? Why or why not?
 
-
-
+# La multiplicación también se realiza elemento por elemento :o
+e==a
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
+d_max=d.max()
+d_min=d.min()
+d_mean=d.mean()
 
-
+print("Max",d_max)
+print("Min",d_min)
+print("Mean",d_mean)
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
+f=np.empty((2,3,5))
+f
 
 
 """
@@ -75,7 +86,24 @@ In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
 
-
+f=np.where(d==d_max,100,np.where(d == d_min,0,np.where((d<d_mean)&(d>d_min),25,np.where((d<d_max)&(d>d_mean),75,100))))
+# No me salió sin Numpy ;(
+# for i in d:
+#     for j in i:
+#         for k in j:
+#             if k==d_min:
+# #                 f[i][j][k]=0
+#             elif k==d_max:
+# #                 f[i][j][k]=100
+#             elif k==d_mean:
+# #                 f[i][j][k]=50
+#             elif (k>d_min and k<d_mean):
+# #                 f[i][j][k]=25
+#             elif (k>d_mean and k<d_max):
+# #                 f[i][j][k]=75
+#             else:
+# #                 f[i][j][k]=":(" ''
+f
 
 
 """
@@ -99,6 +127,9 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
 
+# Parece que sí, solo que ningún valor es igual a la media (creo que estaba dificil atinarle)
+print(d)
+print(f)
 
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
@@ -112,3 +143,7 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+
+f=np.where(d==d_max,"E",np.where(d == d_min,"A",np.where((d<d_mean)&(d>d_min),"B",np.where((d<d_max)&(d>d_mean),"D",100))))
+print(d)
+print(f)
