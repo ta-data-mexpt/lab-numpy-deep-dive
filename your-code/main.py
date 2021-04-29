c73e42bc02044b8532abb5db0c47f8dc5ea51cc3
Miguel Diaz <miguel.d.ferrusca@gmail.com>
2021-04-28 19:02:44 -0500

    partial solved

--- your-code/main.py
+++ your-code/main.py
@@ -1,68 +1,75 @@
 #1. Import the NUMPY package under the name np.
 
+import numpy as np
+
 
 
 #2. Print the NUMPY version and the configuration.
 
-
+print(np.__version__)
+print(np.show_config())
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
-
+a = np.random.random((2,3,5))
 
 #4. Print a.
 
-
+print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
+b = np.ones((5,2,3))
 
 
 #6. Print b.
 
-
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
+a.size == b.size
 
 
 #8. Are you able to add a and b? Why or why not?
 
-
+# np.add(a,b) -> Array shape is not the same so tehy can't be added. Error: ValueError: operands could not be broadcast together with shapes (2,3,5) (5,2,3) 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
+c = np.transpose(b, (1,2,0))
+
+#10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now? Since arrays now have the same shape, elements with shared posotion between arrays may be added.
+
+d = np.add(a,c)
+
+#11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain. Each item in array d is the item in the array a plus one.
 
 
-#10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
-
-
-#11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-
-
-
+print(a)
+print(d)
 
 #12. Multiply a and c. Assign the result to e.
 
+e = np.multiply(a,d)
 
-
-#13. Does e equal to a? Why or why not?
+#13. Does e equal to a? Why or why not? - yes because every element in c is == 1, so all a array is being multiplied by 1.
 
 
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
+d_max = d.max()
+d_min = d.min()
+d_mean = d.mean()
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
+f = np.zeros((2,3,5))
 
 
 """
