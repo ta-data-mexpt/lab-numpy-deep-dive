0a7becf89c614f6f3f61bf554f3b8662dcce59a4
Lucio_Gutierrez <luciogutierrez@users.noreply.github.com>
2021-04-29 13:58:33 -0500

    End of numpy deep dive lab

--- your-code/main.py
+++ your-code/main.py
@@ -1,69 +1,67 @@
-#1. Import the NUMPY package under the name np.
+# 1. Import the NUMPY package under the name np.
+import numpy as np
+
+# 2. Print the NUMPY version and the configuration.
+print(np.__version__)
 
 
-
-#2. Print the NUMPY version and the configuration.
-
-
-
-#3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
+# 3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
+a = np.random.randint(100, size=[2, 3, 5])
+
+# 4. Print a.
+print(a)
+
+# 5. Create a 5x2x3 3-dimensional array with all values equaling 1.
+# Assign the array to variable "b"
+b = np.ones([5, 2, 3])
+
+# 6. Print b.
+print(b)
 
 
-#4. Print a.
+# 7. Do a and b have the same size? How do you prove that in Python code?
+print(a)
+a = b.copy()
+print(a)
 
 
-
-#5. Create a 5x2x3 3-dimensional array with all values equaling 1.
-#Assign the array to variable "b"
-
+# 8. Are you able to add a and b? Why or why not?
+# No, because they have diferentes dimensions
 
 
-#6. Print b.
+# 9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
+c = b.reshape(2, 3, 5)
+print(c)
 
 
+# 10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
+d = a+c
+print(d)
+both arrays have the same structure
 
-#7. Do a and b have the same size? How do you prove that in Python code?
+# 11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
+print(a)
+print(d)
 
+# 12. Multiply a and c. Assign the result to e.
+d = a * c
+print(d)
 
+# 13. Does e equal to a? Why or why not?
+# No, because the array result from multiply is a float array
 
-
-#8. Are you able to add a and b? Why or why not?
-
-
-
-#9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
-
-
-#10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
-
-
-#11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-
-
-
-
-#12. Multiply a and c. Assign the result to e.
+# 14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
+d_max = d.max()
+d_min = d.min()
+d_mean = d.mean()
 
 
-
-#13. Does e equal to a? Why or why not?
-
-
-
-
-#14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
-
-
-
-
-#15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
-
-
-
+# 15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
+f = np.empty(d.shape)
+g = np.empty([2,3,5], dtype=object)
+# print(f)
 
 """
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
@@ -74,9 +72,18 @@ Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
-
-
-
+for i, x in np.ndenumerate(d):
+    if x > d_min and x < d_mean:
+        f[i] = 25
+    if x > d_mean and x < d_max:
+        f[i] = 75
+    if x == d_mean:
+        f[i] = 50
+    if x == d_min:
+        f[i] = 0
+    if x == d_max:
+        f[i] = 100
+print(f)
 
 """
 #17. Print d and f. Do you have your expected f?
@@ -98,7 +105,7 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
-
+# Yes
 
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
@@ -112,3 +119,16 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+for i, x in np.ndenumerate(d):
+    if x > d_min and x < d_mean:
+        g[i] = 'D'
+    if x > d_mean and x < d_max:
+        g[i] = 'B'
+    if x == d_mean:
+        g[i] = 'E'
+    if x == d_min:
+        g[i] = 'A'
+    if x == d_max:
+        g[i] = 'C'
+print(g)
+print()
