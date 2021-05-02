2e080e1193f3efe7d176a6b9d9d22157bf321ab4
Ramon Hernandez Maldonado <ramon.hernandez@ferromex.mx>
2021-05-02 06:04:27 -0500

    Solucion lab rhm

--- your-code/main.py
+++ your-code/main.py
@@ -1,69 +1,77 @@
 #1. Import the NUMPY package under the name np.
-
+import numpy as np
 
 
 #2. Print the NUMPY version and the configuration.
-
+print(np.__version__)
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
-
+a = np.random.rand(2,3,5)
 
 
 #4. Print a.
-
+print(a)
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
+b = np.ones((5,3,2))
 
 
 #6. Print b.
-
+print(b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
-
-
+if a.size == b.size:
+    print('Los conjuntos son iguales')
+elif a.size < b.size:
+    print('El conjunto a es menor al b')
+else:
+    print('El conjunto a es mayor al b')
 
 
 #8. Are you able to add a and b? Why or why not?
-
+print('Las dimesiones no coinciden')
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
+c = np.transpose(np.reshape(b, (5, 3, 2)))
+print(c)
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
+d = np.add(a,c) 
+print(d)
 
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
+print(a)
+print('\n')
+print(d)
 
-
-
+print('La relación es la diferencia en una unidad de ya que el arreglo a generó numeros menores a 1')
 
 #12. Multiply a and c. Assign the result to e.
-
-
+e = np.multiply(a, c)
+print(e)
 
 #13. Does e equal to a? Why or why not?
-
-
-
+print('Son iguales ya que el arreglo C contene elementos con valor 1')
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
-
-
-
+d_max = np.max(d)
+d_min = np.min(d)
+d_mean = np.mean(d)
+print(d_max)
+print(d_min)
+print(d_mean)
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
-
-
-
+f = np.empty((2,3,5))
+print(f)
 
 """
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
@@ -74,8 +82,8 @@ Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
-
-
+f = np.where((d > d_min) & (d < d_mean), 25, np.where((d > d_mean) & (d < d_max), 75, np.where((d == d_mean), 50, 0)))
+print(f)
 
 
 """
