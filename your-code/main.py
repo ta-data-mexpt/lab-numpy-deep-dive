ad5ffab22c95dcdaa04f76c338eae8429718a41f
Ricardo Reyes <reyes.valdes.ricardo@gmail.com>
2021-05-03 18:34:34 -0400

    lab-numpy-deep-dive[Ricardo Reyes]

--- your-code/main.py
+++ your-code/main.py
@@ -1,67 +1,75 @@
 #1. Import the NUMPY package under the name np.
-
+import numpy as np
 
 
 #2. Print the NUMPY version and the configuration.
-
+np.version.version
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
-
+a = np.random.random((2,3,5))
 
 
 #4. Print a.
-
+print(a)
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
+b = np.pnes((5,2,3))
 
 
 #6. Print b.
-
+print(b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
-
+a.size == b.size 
 
 
 
 #8. Are you able to add a and b? Why or why not?
-
+# No they have different shapes 
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
+c = b.reshape(2,3,5)
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
+# It works now beacause they boyh have the same shape 
 
+d = a + c
 
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
+# Both arrays have the same number of elements, shape and type of item (floats) 
 
-
+print(a)
+print(d)
 
 
 #12. Multiply a and c. Assign the result to e.
-
+e = a * c
 
 
 #13. Does e equal to a? Why or why not?
-
+# Yes, because all their values are the sam
+a == e
 
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
+d_max = np.amax(d)
+d_min = np.amin(d)
+d_mean = np.mean(d)
 
 
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
-
+f = np.empty((2,3,5))
 
 
 
@@ -75,7 +83,9 @@ In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
 
-
+# https://stackoverflow.com/questions/57501591/how-to-add-elements-to-np-array-with-several-conditions
+f = [[[25 if d_min < item < d_mean else 75 if d_mean < item < d_max else 50 if item == d_mean else 0 if item == d_min else 100 for item in lists] for lists in list_list] for list_list in d]
+f = np.array(f)
 
 
 """
@@ -98,7 +108,7 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
-
+print(f)
 
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
@@ -112,3 +122,6 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+f = [[["A" if d_min < item < d_mean else "B" if d_mean < item < d_max else "C" if item == d_mean else "D" if item == d_min else "E" for item in lists] for lists in list_lists] for list_lists in d]
+f = np.array(f)
+print(f)
