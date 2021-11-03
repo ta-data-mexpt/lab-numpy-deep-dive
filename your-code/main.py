bcf4b3f3340a74e1e06e537b18b57b342b5c315a
franciscosolano1 <88449161+franciscosolano1@users.noreply.github.com>
2021-11-03 10:36:52 -0600

    commiting numpy lab

--- your-code/main.py
+++ your-code/main.py
@@ -23,34 +23,39 @@ b=np.array(5,2,3)
 
 
 #6. Print b.
-
+print(b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
+if a ==b:
+    x=True
+else:
+    x=False
+print(x)
 
 
 #8. Are you able to add a and b? Why or why not?
 
-
+#yes you can add a and b as they are both lists of integers. 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
+c=np.transpose(a)
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+d = a+c
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-
+print(a)
+print(d)
 
 
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e= a*c
 
 #13. Does e equal to a? Why or why not?
 
@@ -59,11 +64,13 @@ b=np.array(5,2,3)
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
+np.max(d)
+np.min(d)
+np.mean(d)
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
-
+np.empty(d)
 
 
 
