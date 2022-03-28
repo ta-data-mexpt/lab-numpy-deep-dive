9c6e262b1cb1e2d969ea838d3b264861e5e32555
michesquivel <90282268+michesquivel@users.noreply.github.com>
2022-03-27 19:19:28 -0600

    subiendo lab por fin

--- your-code/main.py
+++ your-code/main.py
@@ -1,69 +1,56 @@
 #1. Import the NUMPY package under the name np.
-
-
-
+import numpy as np
+import random 
 #2. Print the NUMPY version and the configuration.
-
-
-
+print(np.__version__)
+print(np.show_config())
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
-
-
-
+a = np.random.random((2,3,5))
+lista = [[[random.random() for i in range(5)] for i in range(3)] for i in range(2)]
+print(lista)
+a2 = np.array(lista)
+print('a2 = ', a2)
+a3 = np.random.randint((2,3,5))
 #4. Print a.
-
-
-
+print('a=', a)
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
-
-
+b = np.array([[[1 for i in range(3)] for i in range(2 )] for i in range(5)])
 #6. Print b.
-
-
-
+print('b = 2',b)
 #7. Do a and b have the same size? How do you prove that in Python code?
-
-
-
-
+print(np.size(a))
+print(np.size (b))
+print('Shape a :',np.shape(a))
+print('Shape b :',np.shape(b))
+print('Have a and b the same size:',np.size(a) == np.size(b))
 #8. Are you able to add a and b? Why or why not?
-
-
-
+#d = a+b
+#They have de same size but not the same shape because of it, The sum cann't be process it. 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
-
-
+c = np.transpose(b,(1,2,0))
+print('Shape c :',np.shape(c))
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
-
-
+d = a + c
+print('c = ',c)
+#It works because withe the transpose on the transformation of b equalls the shape of a, now the values can be sum. 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-
-
-
-
+print('a =',a, '\n d = ',d)
 #12. Multiply a and c. Assign the result to e.
-
-
-
+e = a*c
 #13. Does e equal to a? Why or why not?
-
-
-
+print(e == a)
+#Because c is made of ones. 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
-
-
-
-
+d_min = np.min(d)
+d_max = np.max(d)
+d_mean = np.mean(d)
+print(d_min,d_max,d_mean)
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
-
-
-
+f=np.empty((2,3,5))
+print('f =',f)
 
 """
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
@@ -74,31 +61,38 @@ Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
-
-
-
-
+for i in range(2) :
+    for j in range(3):
+        for k in range(5):
+            if d[i,j,k]>d_min and d[i,j,k]<d_mean :
+                f[i,j,k]=25
+            elif d[i,j,k]>d_mean and d[i,j,k]<d_max :
+                f[i,j,k]=75
+            elif d[i,j,k]==d_mean:
+                f[i,j,k]=50
+            elif d[i,j,k]==d_min:
+                f[i,j,k]=0
+            elif d[i,j,k]==d_max:
+                f[i,j,k]=100
 """
 #17. Print d and f. Do you have your expected f?
 For instance, if your d is:
 array([[[1.85836099, 1.67064465, 1.62576044, 1.40243961, 1.88454931],
         [1.75354326, 1.69403643, 1.36729252, 1.61415071, 1.12104981],
         [1.72201435, 1.1862918 , 1.87078449, 1.7726778 , 1.88180042]],
-
        [[1.44747908, 1.31673383, 1.02000951, 1.52218947, 1.97066381],
         [1.79129243, 1.74983003, 1.96028037, 1.85166831, 1.65450881],
         [1.18068344, 1.9587381 , 1.00656599, 1.93402165, 1.73514584]]])
-
 Your f should be:
 array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 75.,  75.,  25.,  25.,  25.],
         [ 75.,  25.,  75.,  75.,  75.]],
-
        [[ 25.,  25.,  25.,  25., 100.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
-
+print('f = ',f)
+print('d = ',d)
 
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
@@ -106,9 +100,23 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
 array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'D',  'D',  'B',  'B',  'B'],
         [ 'D',  'B',  'D',  'D',  'D']],
-
        [[ 'B',  'B',  'B',  'B',  'E'],
         [ 'D',  'D',  'D',  'D',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+g=np.empty((2,3,5), dtype=str)
+for i in range(2) :
+    for j in range(3):
+        for k in range(5):
+            if d[i,j,k]>d_min and d[i,j,k]<d_mean :
+                g[i,j,k]='B'
+            elif d[i,j,k]>d_mean and d[i,j,k]<d_max :
+                g[i,j,k]='D'
+            elif d[i,j,k]==d_mean:
+                g[i,j,k]='C'
+            elif d[i,j,k]==d_min:
+                g[i,j,k]='A'
+            elif d[i,j,k]==d_max:
+                g[i,j,k]='E'
+print(g)
