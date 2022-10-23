8ceca7993d6255b881af0e9bbd565fb0ce18a037
Montserrat Labrada <mon.lab@usask.ca>
2022-10-22 23:04:53 -0400

    Completed lab numpy

--- your-code/main.py
+++ your-code/main.py
@@ -1,68 +1,72 @@
 #1. Import the NUMPY package under the name np.
 
-
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
-
+print(np.version.version)
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
-
+a = np.random.random([2,3,5])
 
 #4. Print a.
 
-
+print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b=np.ones([5,2,3])
 
 #6. Print b.
 
-
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
-
+a.size
+b.size
+#si tienen el mismo size
 
 #8. Are you able to add a and b? Why or why not?
-
-
+#no porque la configuracion de las matrices es diferente
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
-
+c=b.reshape(2,3,5)
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+d=a+c #porque ahora existe la misma configuracion en matrices a y c
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
+print(a)
 
+print(d)
 
+#'a' y 'd' tienen la mismma configuracion, los valores de 'd' son una unidad mayor a los valores de a
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e=a*c
 
 #13. Does e equal to a? Why or why not?
 
-
+#'e' y 'a' son iguales porque 'e' son los valores de 'a' multiplicados por 1.
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
-
+d_max=np.max(d)
+d_min=np.min(d)
+d_mean=np.mean(d)
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
+f=np.empty([2,3,5])
 
 
 """
@@ -74,9 +78,9 @@ Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
-
-
-
+f=np.where(d==d_max,100,np.where(d==d_min,0,np.where(d==d_mean,50,np.where((d_mean>d)&(d>d_min),25,np.where((d_max>d)&(d>d_mean),75,d)))))
+print(d)
+print(f)
 
 """
 #17. Print d and f. Do you have your expected f?
